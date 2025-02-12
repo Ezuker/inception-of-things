b78fd2d601f65dc397e8a03053adc907cfd33f78
Ezuker <benoit.carolle.pro@gmail.com>
2025-02-12 08:00:12 -0500

    feat: nearly the end of p3

--- p3/install.sh
+++ p3/install.sh
@@ -19,8 +19,8 @@ CLUSTER_NAME=mycluster
 # With option clean, you clean the cluster including the namspaces
 if [ -n "$1" ] && [ "$1" = "clean" ]; then
     echo -e "${BOLD}${RED}Cleaning k3d cluster and deleting namespaces.${RESET}"
-    sudo k3d cluster delete mycluster
-    sudo kubectl get ns --no-headers | awk '{print $1}' | grep -vE '^(default|kube-system|kube-public|kube-node-lease)$' | xargs sudo kubectl delete ns
+    k3d cluster delete mycluster
+    kubectl get ns --no-headers | awk '{print $1}' | grep -vE '^(default|kube-system|kube-public|kube-node-lease)$' | xargs sudo kubectl delete ns
     exit 0
 fi
 
@@ -35,7 +35,7 @@ fi
 
 if ! k3d --version >/dev/null 2>&1; then
     echo -e "${BOLD}${RED}k3d is not installed. Installing now...${RESET}"
-    sudo curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | sudo bash
+    curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 else
     echo -e "${GREEN}k3d is already installed.${RESET}"
 fi
@@ -44,8 +44,8 @@ if ! command -v kubectl >/dev/null 2>&1; then
     echo -e "${BOLD}${RED}kubectl is not installed. Installing now...${RESET}"
     sudo apt-get update && sudo apt upgrade -y
     sudo apt install curl apt-transport-https -y
-    sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-    sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 else
     echo -e "${GREEN}kubectl already installed${RESET}"
 fi
@@ -54,37 +54,50 @@ fi
 # Check if the cluster is already created or not
 if ! k3d cluster list | grep -q "$CLUSTER_NAME"; then
     echo -e "${RED}Cluster not created, creating it now...${RESET}"
-    sudo k3d cluster create $CLUSTER_NAME
+    k3d cluster create $CLUSTER_NAME
     echo -e "${GREEN}Cluster $CLUSTER_NAME created !${RESET}"
 else
     echo -e "${GREEN}Cluster already created${RESET}"
 fi
 
-if [ "$(sudo kubectl config current-context)" != "k3d-$CLUSTER_NAME" ]; then
+if [ "$(kubectl config current-context)" != "k3d-$CLUSTER_NAME" ]; then
     echo -e "${RED}Not using the expected context. Switching...${RESET}"
-    sudo kubectl config use-context "k3d-$CLUSTER_NAME"
+    kubectl config use-context "k3d-$CLUSTER_NAME"
 else
     echo -e "${GREEN}Already using the correct context.${RESET}"
+    kubectl config current-context #aucasou
 fi
 
 
 if [ "$(sudo kubectl get ns | grep -c argocd)" -ne 1 ]; then
     echo -e "${RED}No namespace named argocd. Creating it...${RESET}" 
-    sudo kubectl create namespace argocd
-    sudo kubectl create namespace dev
+    kubectl create namespace argocd
+    kubectl create namespace dev
 fi
-sudo kubectl apply -k .
-sleep 10
-sudo kubectl port-forward svc/argocd-server -n argocd 8080:443 &
 
-if ! brew --version; then
-    git clone https://github.com/Homebrew/brew ~/homebrew
+kubectl apply -k /vagrant/
+
+while [ "$(kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o jsonpath="{.items[0].status.phase}")" != "Running" ]; do
+  echo "Waiting for Argo CD server pod to be ready..."
+  sleep 2
+done
+
+echo "Argo CD server is running! Starting port-forwarding..."
+kubectl port-forward svc/argocd-server -n argocd 8080:443 --address 0.0.0.0 &
+sleep 20
+if ! argocd version >/dev/null 2>&1; then
+    echo "argocd cli not installed... installing it"
+    sudo curl -sSL -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+    sudo chmod +x /usr/local/bin/argocd
+    export SECRET_PASS=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
+    export | grep SECRET_PASS
+    curl http://localhost:8080
+    argocd login https://localhost:8080 --username admin --password $SECRET_PASS --insecure
+    argocd repo add https://github.com/Ezuker/iot-argocd.git
+    argocd app create my-app \
+        --repo https://github.com/Ezuker/iot-argocd.git \
+        --path k8s-manifests \
+        --dest-server https://kubernetes.default.svc \
+        --dest-namespace dev
+    argocd app sync my-app
 fi
-if ! argocd version; then
-    brew install argocd
-fi
-argocd app create <APP_NAME> \
-  --repo <REPO_URL> \
-  --path <PATH_TO_MANIFESTS> \
-  --dest-server <K8S_API_SERVER> \
-  --dest-namespace <NAMESPACE>
